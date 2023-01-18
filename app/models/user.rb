253ccd2c5e8892c357482f460996983fcb7081a9
ishsho <ishsho1203@gmail.com>
2023-01-18 14:43:08 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,             presence: true
+  validates :email,                presence: true
+  validates :encrypted_password,   presence: true
+  validates :family_name_japanese, presence: true
+  validates :first_name_japanese,  presence: true
+  validates :family_name_katakana, presence: true
+  validates :first_name_katakana,  presence: true
+  validates :birthday,             presence: true
 end
