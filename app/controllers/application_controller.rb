7c114ec7d0dca307a33bb11af5658baea1b2ada7
ishsho <ishsho1203@gmail.com>
2023-01-18 17:33:07 +0900

    エラー文の表示〜テストコード準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
 
   private
@@ -8,5 +10,11 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'ishsho' && password == '1203'
     end
+  end
+
+  def configure_permitted_parameters
+    binding.pry
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name_japanese,
+       :first_name_japanese, :family_name_katakana, :first_name_katakana, :birthday])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :installs
   root to: 'items#index'
   resources :items, only: [:index]
 end
