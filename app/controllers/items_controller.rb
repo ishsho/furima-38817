47c5171d8e3ba3340e9f282e33f00db2f4b079fc
ishsho <ishsho1203@gmail.com>
2023-01-19 18:00:01 +0900

    商品出品ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,20 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+  
+  def new
+    @items = Item.new
+  end
 
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :content, 
+      :category_id, :state_id, :load_id, :area_id, :shipping_date_id,:price).merge(
+      user_id: current_user.id)
 
 end
