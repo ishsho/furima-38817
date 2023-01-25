df1dc442c833a3e1da72822cd4881bdc3c5e2f51
ishsho <ishsho1203@gmail.com>
2023-01-25 22:15:32 +0900

    テストコード作成中

---
+++ spec/factories/order_transacts.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_transact do
+    postal_code         {'111-1111'}
+    area_id             {'2'}
+    municipalities      {'横浜市'}
+    adress              {'1111'}
+    building            {''}
+    phone               {'1111111111'}
+  end
+end
---
+++ spec/models/order_transact_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe OrderTransact, type: :model do
+  describe '商品購入機能' do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_transact = FactoryBot.build(:order_transact, user_id: user.id, item_id: item.id)
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+      end
+
+      it 'buildingは空でも保存できること' do
+      end
+
+    end
+
+    context '内容に問題がある場合' do
+      it 'postal_codeが空だと保存できないこと' do
+      end
+
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+      end
+
+      it 'area_idを選択していないと保存できないこと' do
+      end
+
+      it 'municipalitiesが空だと保存できないこと' do
+      end
+
+      it 'adressが空だと保存できないこと' do
+      end
+
+      it 'phoneが空だと保存できないこと' do
+      end
+
+      it 'phoneが9桁以下では保存できないこと' do
+      end
+
+      it 'phoneが12桁以上では保存できないこと' do
+      end
+
+      it 'phoneにハイフン(-)が記載されると保存できないこと' do
+      end
+
+      it 'userが紐付いていないと保存できないこと' do
+      end
+
+      it 'itemが紐付いていないと保存できないこと' do
+      end
+
+    end
+  end
+end
