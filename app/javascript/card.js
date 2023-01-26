acf9f9a190959cd405087681debcfdaf1f88a40c
ishsho <ishsho1203@gmail.com>
2023-01-26 16:17:26 +0900

    クレジットカード、トークン取得まで

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,12 +1,12 @@
 const pay = () => {
-  const payjp = Payjp('')
+  const payjp = Payjp('pk_test_27b27f1036bcb5d0ccde709d')
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
 
   numberElement.mount('#card-number');
-  expiryElement.mount('#card-exp-month','#card-exp-year');
+  expiryElement.mount('#card-exp');
   cvcElement.mount('#card-cvc');
 
   const submit = document.getElementById("button");
@@ -18,7 +18,9 @@ const pay = () => {
       if (response.error) {
       } else {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
     });
   });
