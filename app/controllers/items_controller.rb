0343e84be5711db2aaed1be8090aabb74c08ec5d
ishsho <ishsho1203@gmail.com>
2023-01-26 21:28:37 +0900

    追記２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,8 @@ class ItemsController < ApplicationController
   def edit
     if current_user.id != @item.user_id 
       redirect_to root_path
+    elsif @item.order
+      redirect_to root_path
     end
   end
 
