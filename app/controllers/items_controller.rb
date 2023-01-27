18b44e926d917f9a15bc2f0013998da30048b51c
ishsho <ishsho1203@gmail.com>
2023-01-27 11:32:09 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id != @item.user_id 
-      redirect_to root_path
-    elsif @item.order
+    if current_user.id != @item.user_id || @item.order
       redirect_to root_path
     end
   end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
 
   def index
     @order_transact = OrderTransact.new
-    if @item.order 
+    if @item.order  || @item.user == current_user
       redirect_to root_path
     end
   end
